0b16def15a3bc7dcadf5687ae841e4fde8df3826
jotagep <jotagmzn@gmail.com>
2020-06-05 17:43:00 +0200

    Update mocks

---
+++ lib/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const Color colorYoutube = Color(0xffFF000);
---
+++ lib/mock.dart
@@ -0,0 +1,7 @@
+List<String> mockTitles = [
+  'How I Sold My Python/Django Website for \$41,000',
+  '¿Por qué SPACEX está haciendo historia?',
+  'Vue.js: The Documentary',
+  'DIRECTO: Última hora sobre el coronavirus en España',
+  'Como se vive como desarrollador en un país desarrollado (Nueva Zelanda)'
+];
